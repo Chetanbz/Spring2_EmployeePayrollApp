650fd8658b1dd17db90c175ebc9c201efdda65ae
chetan <zambare.c123@gmail.com>
2021-01-27 15:04:19 +0530

    [chetan] diffrent Environment setup

--- src/main/java/com/app/employeeapp/EmployeeappApplication.java
+++ src/main/java/com/app/employeeapp/EmployeeappApplication.java
@@ -1,13 +1,20 @@
 package com.app.employeeapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeeappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeeappApplication.class, args);
+        System.out.print("hello application started");
+        ApplicationContext context = SpringApplication.run(EmployeeappApplication.class, args);
+        log.debug("Employee Payroll App Started in {} Environment ",
+                context.getEnvironment().getProperty("environment"));
+        
     }
 
 }
