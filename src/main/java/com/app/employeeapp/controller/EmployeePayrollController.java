fe404d5e435a9bf0d10392d950811f8dfd20bcdb
chetan <zambare.c123@gmail.com>
2021-01-27 14:37:35 +0530

    [chetan] Added Data Stored in LIst

--- src/main/java/com/app/employeeapp/controller/EmployeePayrollController.java
+++ src/main/java/com/app/employeeapp/controller/EmployeePayrollController.java
@@ -19,43 +19,48 @@ public class EmployeePayrollController {
     @Autowired
     private IEmployeePayrollService employeePayrollService;
 
-    @RequestMapping(value = {"", "/", "/get"})
-    public ResponseEntity<ResponseDTO> getEmployeePayrollData() {
+    @RequestMapping(value = {"","/","/get"})
+    public ResponseEntity<ResponseDTO> getEmployeePayrollData(){
+        System.out.println("Get,1");
         List<EmployeePayrollData> empdataList = null;
         empdataList = employeePayrollService.getEmployeePayrollData();
         ResponseDTO respDTO = new ResponseDTO("Get Call Successful", empdataList);
-        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
     }
 
     @GetMapping("/get/{empId}")
-    public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId") int empId) {
-
-        EmployeePayrollData empData = null;
+    public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId") int empId){
+        System.out.println("Get,2");
+        EmployeePayrollData empData =null;
         empData = employeePayrollService.getEmployeePayrollDataById(empId);
         ResponseDTO respDTO = new ResponseDTO("Get call Sucess for Id:", empData);
-        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
-        EmployeePayrollData empData = null;
+    public ResponseEntity<ResponseDTO> addEmployeePayrollData( @RequestBody EmployeePayrollDTO employeePayrollDTO){
+        System.out.println("post");
+        EmployeePayrollData empData =null;
         empData = employeePayrollService.createEmployeePayrollData(employeePayrollDTO);
         ResponseDTO respDTO = new ResponseDTO("Created Employee Payroll Data for: ", empData);
-        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
     }
 
-    @PutMapping("/update")
-    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
-        EmployeePayrollData empData = null;
-        empData = employeePayrollService.updateEmployeePayrollData(employeePayrollDTO);
+    @PutMapping("/update/{empId}")
+    public ResponseEntity<ResponseDTO> updateEmployeePayrollData( @PathVariable("empId") int empId,
+                                                                  @RequestBody EmployeePayrollDTO employeePayrollDTO){
+        System.out.println("Put");
+        EmployeePayrollData empData =null;
+        empData = employeePayrollService.updateEmployeePayrollData(empId,employeePayrollDTO);
         ResponseDTO respDTO = new ResponseDTO("Update Employee Payroll Data for :", empData);
-        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{empId}")
-    public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
+    public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId){
+        System.out.println("delete");
         employeePayrollService.deleteEmployeePayrollData(empId);
-        ResponseDTO respDTO = new ResponseDTO("Delete  Sucessfully :", "Deleted Id: " + empId);
-        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+        ResponseDTO respDTO = new ResponseDTO("Delete  Sucessfully :","Deleted Id: " + empId);
+        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
     }
 }
--- src/main/java/com/app/employeeapp/services/IEmployeePayrollService.java
+++ src/main/java/com/app/employeeapp/services/IEmployeePayrollService.java
@@ -14,7 +14,8 @@ public interface IEmployeePayrollService {
 
     EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO);
 
-    EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO);
+    EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO);
+
 
     void deleteEmployeePayrollData(int empId);
 
